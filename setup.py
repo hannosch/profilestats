854268c9f17e481c7982796859408a8b7f36ee4b
Hanno Schlichting <hanno@jarn.com>
2009-01-31 16:57:52 +0000

    Actually check-in stuff

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='profilestats',
+    version='1.1dev',
+    author='Hanno Schlichting',
+    author_email='hannosch@plone.org',
+    url='http://pypi.python.org/pypi/profilestats',
+    keywords='profile kcachegrind',
+    description='Decorator for profiling individual functions and converting '
+                'profiling data to the kcachegrind format.',
+    long_description=open("README.txt").read(),
+    classifiers = [
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='BSD',
+    install_requires=[
+        'setuptools',
+        'pyprof2calltree',
+    ],
+    py_modules=['profilestats'],
+    zip_safe=False,
+)
