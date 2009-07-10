e2424122e43bf3a49bcb1f0e247fe406199968dc
Hanno Schlichting <hanno@jarn.com>
2009-07-10 18:29:12 +0000

    Update version after release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='profilestats',
-    version='1.0.1',
+    version='1.0.2dev',
     author='Hanno Schlichting',
     author_email='hannosch@plone.org',
     url='http://pypi.python.org/pypi/profilestats',
