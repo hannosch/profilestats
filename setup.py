88fe745710678fbed243de5ea177de6dc251b8bb
Hanno Schlichting <hanno@jarn.com>
2011-02-13 20:06:02 +0100

    Prepare profilestats 1.0.2.

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
 from setuptools import setup
 
 setup(
     name='profilestats',
-    version='1.0.2dev',
+    version='1.0.2',
     author='Hanno Schlichting',
-    author_email='hannosch@plone.org',
+    author_email='hanno@hannosch.eu',
     url='http://pypi.python.org/pypi/profilestats',
     keywords='profile kcachegrind',
     description='Decorator for profiling individual functions and converting '
@@ -15,7 +14,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
     ],
     license='BSD',
     install_requires=[
