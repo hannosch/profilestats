952e3a59c77d73b15a713a2170bb3a012ce58316
Hanno Schlichting <hanno@hannosch.eu>
2013-11-27 14:52:27 +0100

    add and wire up a changelog

--- README.rst
+++ README.rst
@@ -1,5 +1,6 @@
-Overview
-========
+============
+profilestats
+============
 
 This module contains a simple helper decorator which allows you to profile
 a function and write the profiling data to the file system in both the pstats
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import os
+
 from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.rst')) as f:
+    CHANGES = f.read()
 
 setup(
     name='profilestats',
@@ -9,7 +17,7 @@ setup(
     keywords='profile kcachegrind',
     description='Decorator for profiling individual functions and converting '
                 'profiling data to the kcachegrind format.',
-    long_description=open("README.txt").read(),
+    long_description=README + '\n\n' + CHANGES,
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
